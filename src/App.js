177ca2647297746c27b83b9cc9ee87dad723da91
Vansh Sharma <vanshsharma.6460@gmail.com>
2024-11-20 17:56:26 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
-import { auth } from './firebase';
 
-// Protected Route Component
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = auth.currentUser;
-  
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+          {/* Public routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          
+          {/* Default redirect to home*/}
+          <Route path="/" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
   );
